9a7fe6284fe41dcdf763a545052b80aeb38e460b
Nagarajayya Hiremath <Nagaraj.Hiremath@aa.com>
2020-06-30 13:36:18 -0500

    Features/fastlane cd fix (#7)
    * fastlane changes
    
    * added and modified Fastfile - gym entry
    
    * jenkins file changes

--- aae_mobile/ios/Flutter/flutter_export_environment.sh
+++ aae_mobile/ios/Flutter/flutter_export_environment.sh
@@ -1,22 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-<<<<<<< Updated upstream
-export "FLUTTER_ROOT=/Users/aa616058/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/aa616058/Documents/AE/essentials-ui/aae_mobile"
+export "FLUTTER_ROOT=/Users/nagarajhiremath/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/nagarajhiremath/Documents/AmericanEssentials_mycode/essentials-ui/aae_mobile"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=/Users/aa616058/Development/flutter/bin/cache/artifacts/engine/ios"
-=======
-export "FLUTTER_ROOT=/Users/200989/dev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/200989/dev_projects/american-essentials/essentials-ui/aae_mobile"
-export "FLUTTER_TARGET=/Users/200989/dev_projects/american-essentials/essentials-ui/aae_mobile/lib/main.dart"
-export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=/Users/200989/dev/flutter/bin/cache/artifacts/engine/ios"
->>>>>>> Stashed changes
+export "FLUTTER_FRAMEWORK_DIR=/Users/nagarajhiremath/development/flutter/bin/cache/artifacts/engine/ios-release"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
-export "TRACK_WIDGET_CREATION=true"
