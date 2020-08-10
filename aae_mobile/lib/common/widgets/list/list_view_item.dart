f8a654978b81aff4826a4de8974fd030d0c61822
Asif Mohammed <asif.mohammed@aa.com>
2020-08-10 13:59:08 -0500

    Bryce changes - Fix for text sizing issue.

--- aae_mobile/lib/common/widgets/list/list_view_item.dart
+++ aae_mobile/lib/common/widgets/list/list_view_item.dart
@@ -34,14 +34,15 @@ class ListViewItem extends StatelessWidget {
     @required String title,
     @required String body,
     @required String author,
+    @required BuildContext context,
     GestureTapCallback onTapped,
   }) : this(
-            image: image,
-            infoPanelChildren: <Widget>[
-              _buildTitle(title),
-              _buildShortBody(body, author),
-            ],
-            onTapped: onTapped);
+      image: image,
+      infoPanelChildren: <Widget>[
+        _buildTitle(title),
+        _buildShortBody(context, body, author),
+      ],
+      onTapped: onTapped);
 
   @override
   Widget build(BuildContext context) {
@@ -64,21 +65,24 @@ class ListViewItem extends StatelessWidget {
 //        height: 300,
         child: ClipRRect(
           borderRadius: BorderRadius.circular(3.0),
-          child: GestureDetector(
-            child: _buildListItemRow(),
-            onTap: onTapped,
+          child: MediaQuery(
+            data: MediaQueryData(textScaleFactor: 1.0),
+            child: GestureDetector(
+              child: _buildListItemRow(context),
+              onTap: onTapped,
+            ),
           ),
         ),
       ),
     );
   }
 
-  Widget _buildListItemRow() {
+  Widget _buildListItemRow(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         _buildImage(image),
-        _buildInfoPanel(infoPanelChildren)
+        _buildInfoPanel(context, infoPanelChildren)
       ],
     );
   }
@@ -99,13 +103,14 @@ class ListViewItem extends StatelessWidget {
     );
   }
 
-  Widget _buildInfoPanel(List<Widget> children) {
+  Widget _buildInfoPanel(BuildContext context, List<Widget> children) {
     return Expanded(
       child: Container(
+        width: double.infinity,
         color: AaeColors.white,
         padding: EdgeInsets.symmetric(horizontal: 14.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: children,
         ),
       ),
@@ -147,9 +152,10 @@ class ListViewItem extends StatelessWidget {
     );
   }
 
-  static Widget _buildShortBody(String body, author) {
+  static Widget _buildShortBody(BuildContext context, String body, author) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
         Padding(
           padding: const EdgeInsets.only(bottom: 4.0, top: 6.0),
