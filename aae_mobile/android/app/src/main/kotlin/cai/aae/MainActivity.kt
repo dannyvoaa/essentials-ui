2902b7be8e710ee324bffd9e059acabbc4ed8df1
nagaraj hiremath <nagaraj.hiremath@aa.com>
2020-10-02 11:19:46 -0500

    Android MainActivity changes

--- aae_mobile/android/app/src/main/kotlin/cai/aae/MainActivity.kt
+++ aae_mobile/android/app/src/main/kotlin/cai/aae/MainActivity.kt
@@ -5,15 +5,20 @@ import android.os.Bundle
 import io.flutter.app.FlutterActivity
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
-
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 
 class MainActivity : FlutterActivity() {
+
+    // firebase Analytics
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
 
     private val CHANNEL = "aae.cai/channel"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         GeneratedPluginRegistrant.registerWith(this)
+        FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(true)
 
     }
 }
