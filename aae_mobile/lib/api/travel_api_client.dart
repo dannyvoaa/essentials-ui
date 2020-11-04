a122d7fbac4e1784bad8bdf2200ef2ab615e57e9
Danny Vo <danny.vo@aa.com>
2020-11-04 12:56:04 -0600

    Updated flight status url format

--- aae_mobile/lib/api/travel_api_client.dart
+++ aae_mobile/lib/api/travel_api_client.dart
@@ -17,8 +17,7 @@ class TravelServiceApi {
   static const travelReservationsEndpoint = '$baseUrl/reservations';
   static const priorityListEndpoint = '$baseUrl/prioritylist';
 
-  final travelFlightStatusEndpoint =
-      'https://us-south.functions.cloud.ibm.com/api/v1/web/AA-CorpTech-Essentials_dev/travel/flightstatus';
+  final travelFlightStatusEndpoint = '$baseUrl/flightstatus';
 
   final dateFormatter = DateFormat("yyyy-MM-dd");
 
@@ -73,11 +72,13 @@ class TravelServiceApi {
     }
   }
 
-  Future<PriorityList> getPriorityList(String origin, int flightNum, DateTime date) async {
+  Future<PriorityList> getPriorityList(
+      String origin, int flightNum, DateTime date) async {
     _log.info("initiating priority list request: $origin $flightNum $date");
 
     Map<String, String> headers = _getRequestHeaders("72000027");
-    String constructedUrl = "$priorityListEndpoint/$origin/$flightNum/${dateFormatter.format(date)}";
+    String constructedUrl =
+        "$priorityListEndpoint/$origin/$flightNum/${dateFormatter.format(date)}";
     _log.info(constructedUrl);
 
     var response;
@@ -90,23 +91,22 @@ class TravelServiceApi {
     }
 
     if (response.statusCode == 200) {
-
       _log.info("priority list request successful");
       _log.info(response.body);
-      return serializers.deserializeWith(PriorityList.serializer, jsonDecode(response.body));
-
+      return serializers.deserializeWith(
+          PriorityList.serializer, jsonDecode(response.body));
     } else {
       throw Exception(
           'failed to load the priority list.\n ${response.body} - ${response.statusCode} - ${response.headers["error"]}');
     }
   }
 
-  Future<FlightStatus> getFlightStatus(
-      String employeeId, String flightNumber, String origin, String date) async {
+  Future<FlightStatus> getFlightStatus(String employeeId, String flightNumber,
+      String origin, String date) async {
     Map<String, String> headers = _getRequestHeaders(employeeId);
-    final response = await httpClient.get(
-        travelFlightStatusEndpoint + '/' + flightNumber + '/' + origin + '/' + date,
-        headers: headers);
+    String constructedUrl =
+        "$travelFlightStatusEndpoint/$origin/$flightNumber/$date";
+    final response = await httpClient.get(constructedUrl, headers: headers);
     if (response.statusCode == 200) {
       _log.info("Flight Status API request successful");
       _log.info(response.body);
