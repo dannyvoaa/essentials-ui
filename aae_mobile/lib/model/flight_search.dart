c8e5efa858a1f51dc7843560d3047911cc307e2e
Danny Vo <danny.vo@aa.com>
2020-11-25 11:11:26 -0600

    Added navigation for ios build

--- aae_mobile/lib/model/flight_search.dart
+++ aae_mobile/lib/model/flight_search.dart
@@ -174,34 +174,3 @@ abstract class FlightAirport
 
   static Serializer<FlightAirport> get serializer => _$flightAirportSerializer;
 }
-
-//abstract class FlightLeg
-//    implements Built<FlightSearch, FlightLegBuilder> {
-//  FlightLeg._();
-//
-//  factory FlightLeg([updates(FlightLegBuilder b)]) =
-//  _$FlightLeg;
-//
-//  @BuiltValueSerializer(serializeNulls: true)
-//  @nullable
-//  @BuiltValueField(wireName: 'carrierCode')
-//  String get carrierCode;
-//
-//  @BuiltValueSerializer(serializeNulls: true)
-//  @nullable
-//  @BuiltValueField(wireName: 'flightNumber')
-//  String get flightNumber;
-//
-//  String toJson() {
-//    return json
-//        .encode(serializers.serializeWith(FlightLeg.serializer, this));
-//  }
-//
-//  static FlightLeg fromJson(String jsonString) {
-//    return serializers.deserializeWith(
-//        FlightLeg.serializer, json.decode(jsonString));
-//  }
-//
-//  static Serializer<FlightLeg> get serializer =>
-//      _$flightLegSerializer;
-//}
