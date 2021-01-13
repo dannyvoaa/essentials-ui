3edfb3bdf2537efc5f6652c048cb1e409542f687
Danny Vo <danny.vo@aa.com>
2021-01-13 13:20:21 -0600

    Added null check

--- aae_mobile/lib/travel/component/trips/trips_collection.dart
+++ aae_mobile/lib/travel/component/trips/trips_collection.dart
@@ -14,7 +14,8 @@ class TripsCollection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (this.viewModel == null || this.viewModel.pnrs.length > 0) {
+    if (this.viewModel == null ||
+        (this.viewModel.pnrs != null && this.viewModel.pnrs.length > 0)) {
       return _buildTripsCollection(context);
     } else {
       return _buildNoTripsCollection(context);
