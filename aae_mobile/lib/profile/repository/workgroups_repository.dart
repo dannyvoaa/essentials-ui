8d6eec51fe34fb7cdd19aba021b8de5c18d8ffd6
Ajay <ajay.patel@aa.com>
2021-03-09 20:53:04 -0600

    Fix text wrapping issue

--- aae_mobile/lib/profile/repository/workgroups_repository.dart
+++ aae_mobile/lib/profile/repository/workgroups_repository.dart
@@ -25,7 +25,7 @@ class WorkgroupsRepository {
     listOfWorkgroups.add(Workgroup((b) => b.workgroups = 'Fleet Service'));
     listOfWorkgroups.add(Workgroup((b) => b.workgroups = 'Flight'));
     listOfWorkgroups.add(Workgroup((b) => b.workgroups = 'Flight Service'));
-    listOfWorkgroups.add(Workgroup((b) => b.workgroups = 'Leadership and Support Staff'));
+    listOfWorkgroups.add(Workgroup((b) => b.workgroups = 'Leadership & Support Staff'));
     listOfWorkgroups.add(Workgroup((b) => b.workgroups = 'Premium Guest Services'));
     listOfWorkgroups.add(Workgroup((b) => b.workgroups = 'Reservations'));
     listOfWorkgroups.add(Workgroup((b) => b.workgroups = 'Tech Ops'));
